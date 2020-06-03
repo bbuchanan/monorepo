dd6fa445afe2b710a0e83902ed8698a6c7c9968b
Bill Buchanan <bbuchanan@gmail.com>
2020-06-03 10:59:26 -0400

    added in support for mobile and linked to the common project

---
+++ packages/mobile/App.tsx
@@ -0,0 +1,21 @@
+import { add } from "@monorepo/common";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>1+1={add(1, 1)}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
